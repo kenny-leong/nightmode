92dbc41023feaa698350b2e14b764c4858a5e37a
Kenny Leong <kennyleong2@gmail.com>
2023-01-23 17:24:37 -0500

    Add User logout backend endpoint

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -35,6 +35,17 @@ router.post(
   );
 
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+  // ...
+
 
 
 
