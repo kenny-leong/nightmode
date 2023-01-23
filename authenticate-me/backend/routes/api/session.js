8c762eaed55716fc30694f0c4579bd9cd0070f95
Kenny Leong <kennyleong2@gmail.com>
2023-01-23 17:27:35 -0500

    Add a backend endpoint to get the current user session

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -44,9 +44,22 @@ router.delete(
     }
   );
 
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
   // ...
-
-
 
 
 
