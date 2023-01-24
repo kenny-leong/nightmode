5a29271a42eee0dda0146bffb8f2857ca4596912
Kenny Leong <kennyleong2@gmail.com>
2023-01-24 14:13:34 -0500

    user seed data changed.

--- backend/db/seeders/20230123220234-demo-user.js
+++ backend/db/seeders/20230123220234-demo-user.js
@@ -1,5 +1,5 @@
 'use strict';
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs"); //Phase 3: import bcryprtjs
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
@@ -13,17 +13,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'Demo',
+        lastName: "Lition"
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'Fake',
+        lastName: "User1"
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'Fake',
+        lastName: "User2"
       }
     ], {});
   },
