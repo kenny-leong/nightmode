16a5cb0728242bfae80655fdd86d28aa0b29553f
Kenny Leong <kennyleong2@gmail.com>
2023-01-25 19:30:48 -0500

    new endpoints added to reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,4 +1,4 @@
-//Feature 2: Set up the router
+//Feature 2: Set up the review router
 const express = require('express');
 
 const router = express.Router();
@@ -53,6 +53,122 @@ router.get('/current', requireAuth, async (req, res) => {
 
     return res.json({ Reviews: reviewsArr })
 });
+
+
+// POST /api/reviews/:reviewId/images (Add an img to a review by reviewId)
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+    const currUser = req.user.id;
+    const review = await Review.findByPk(req.params.reviewId);
+
+    //Check if review exists
+    if (!review) {
+        return res.status(404).json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        });
+    }
+
+    // Check if current user has permission to add new img to review
+    if (currUser != review.userId) {
+        return res.status(403).json({
+            message: "Forbidden",
+            statusCode: 403
+        });
+    }
+
+    // Cannot add any more images because there is a maximum of 10 images per resource
+    const reviewImagesCount = await ReviewImage.count({
+        where: { reviewId: review.id }
+    });
+    if (reviewImagesCount >= 10) {
+        return res.status(403).json({
+            message: "Maximum number of iamges for this resource was reached",
+            statusCode: 403
+        });
+    }
+
+    const newImage = await ReviewImage.create({ reviewId: review.id, ...req.body });
+    const { id, url } = newImage;
+
+    return res.json({id, url});
+});
+
+// PUT /api/reviews/:reviewId (Edit review)
+router.put('/:reviewId', requireAuth, async (req, res) => {
+    const currUser = req.user.id;
+    const review = await Review.findByPk(req.params.reviewId);
+
+    //Check if review exists
+    if (!review) {
+        return res.status(404).json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        });
+    }
+
+    // Check permissions of current user
+    if (currUser != review.userId) {
+        return res.status(403).json({
+            message: "Forbidden",
+            statusCode: 403
+        });
+    }
+
+    //Build up error
+    const val = {
+        message: "Validation Error",
+        statusCode: 400
+    }
+
+    //helper fxn error handling
+    function errorHandle(val) {
+        return res.status(400).json(val);
+    }
+
+    //validations
+    const {review: reqReview, stars: stars} = req.body;
+    if (!reqReview) {
+        val.error = "Review text is required";
+        errorHandle(val);
+    } else if (!stars || !Number.isInteger(stars) || stars < 0 || stars > 5) {
+        val.error = "Stars must be an integer from 1 to 5";
+        errorHandle(val);
+    }
+
+    review.update(req.body);
+    return res.json(review);
+});
+
+// DELETE /api/reviews/:reviewId (Delete a review)
+router.delete('/:reviewId', requireAuth, async (req,res) => {
+    const currUser = req.user.id;
+    const review = await Review.findByPk(req.params.reviewId);
+
+    //Check if review exists
+    if (!review) {
+        return res.status(404).json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        });
+    }
+
+    // Check permissions of current user
+    if (currUser != review.userId) {
+        return res.status(403).json({
+            message: "Forbidden",
+            statusCode: 403
+        });
+    }
+
+    review.destroy();
+
+    return res.json({
+        message: "Successfully deleted",
+        statusCode: 200
+    });
+});
+
+
 
 
 
