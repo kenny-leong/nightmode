bbaefa832230af929b465723fdaf518db8f436ea
Kenny Leong <kennyleong2@gmail.com>
2023-01-26 16:28:53 -0500

    spot image delete endpoint created.

--- backend/routes/api/spotImages.js
+++ backend/routes/api/spotImages.js
@@ -8,7 +8,38 @@ const user = require('../../db/models/user');
 const { requireAuth } = require('../../utils/auth');
 
 
+// DELETE /api/spot-images/:imageId (Delete a Spot Image)
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const currUser = req.user.id;
+    const spotImage = await SpotImage.findByPk(req.params.imageId);
 
+    //Check if spot image exists
+    if (!spotImage) {
+        return res.status(404).json({
+            message: "Spot Image couldn't be found",
+            statusCode: 404
+        });
+    }
+
+    //Spot must belong to the current user
+    const spot = await Spot.findByPk(spotImage.spotId);
+
+    if (currUser != spot.ownerId) {
+        return res.status(403).json({
+            message: "Forbidden",
+            statusCode: 403
+        });
+    }
+
+    // remove the spotImage
+    spotImage.destroy();
+
+    res.status(200).json({
+        message: "Successfully deleted",
+        statusCode: 200
+    });
+
+});
 
 
 
