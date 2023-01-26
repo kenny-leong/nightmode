f8a98aecbe31cfa0270ad54a6f438c743237f273
Kenny Leong <kennyleong2@gmail.com>
2023-01-26 18:13:54 -0500

    revised.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -499,7 +499,7 @@ router.post('/:spotId/bookings', requireAuth, async (req, res) => {
     // check user permissions
     if (currUser == spot.ownerId) {
         return res.status(400).json({
-            message: "Booking cannot be made by owner of spot",
+            message: "Forbidden",
             statusCode: 403
         });
     }
