f10a3a1ac8635fe0216e30a004666e06853b018a
Kenny Leong <kennyleong2@gmail.com>
2023-02-10 16:07:23 -0500

    phase 3.

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,33 @@
+// frontend/src/components/Navigation/ProfileButton.js
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown";
+
+  return (
+    <>
+      <button>
+        <i className="fas fa-user-circle" />
+      </button>
+      <ul className="profile-dropdown">
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default ProfileButton;
---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,45 @@
+// frontend/src/components/Navigation/index.js
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import * as sessionActions from '../../store/session';
+import './Navigation.css';
+
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <li>
+        <ProfileButton user={sessionUser} />
+        <button onClick={logout}>Log Out</button>
+      </li>
+    );
+  } else {
+    sessionLinks = (
+      <li>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </li>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+      </li>
+      {isLoaded && sessionLinks}
+    </ul>
+  );
+}
+
+export default Navigation;
