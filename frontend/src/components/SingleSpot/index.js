73fb0be71b3210ada982723d5724380c0d7f7df2
Kenny Leong <kennyleong2@gmail.com>
2023-02-15 11:28:37 -0500

    css styling added to landing.

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -9,7 +9,7 @@ function SingleSpot({ spot }) {
                 <span className='loc'>{spot.city}, {spot.state}</span>
                 <span className='rating'>
                     <i className="fa-sharp fa-solid fa-star"></i>
-                    {!isNaN(spot.avgRating) ? <span>{parseInt(spot.avgRating).toFixed(1)}</span> : <span>New</span>}
+                    {!isNaN(spot.avgRating) ? <span className='int'>{parseInt(spot.avgRating).toFixed(1)}</span> : <span className='int'>New</span>}
                 </span>
             </div>
             <div className="spot-price-div">
