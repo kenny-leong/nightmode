9689668059406d3403b73c0afa7175b9d067f25d
Kenny Leong <kennyleong2@gmail.com>
2023-02-16 03:35:05 -0500

    added default img to ManageSpotItem.

--- frontend/src/components/ManageSpotItem/index.js
+++ frontend/src/components/ManageSpotItem/index.js
@@ -20,6 +20,10 @@ function ManageSpotItem({ spot }) {
             })
     }
 
+    if (spot.previewImage === 'Image not set.') {
+        spot.previewImage = 'https://us.123rf.com/450wm/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016/167492439-no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image.jpg?ver=6';
+    }
+
     return (
         <div className='manage-container'>
             <img className="manage-img" src={`${spot.previewImage}`} alt={`${spot.name}`} />
