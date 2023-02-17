560c54099c53f7547e5f9eda61d3e35e046f25e7
Kenny Leong <kennyleong2@gmail.com>
2023-02-17 00:25:43 -0500

    submit btn logic added.

--- frontend/src/components/PostReview/index.js
+++ frontend/src/components/PostReview/index.js
@@ -1,12 +1,46 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
+import { useEffect, useState } from 'react';
+import './PostReview.css'
 
 
 function PostReview() {
+    const [review, setReview] = useState('');
+    const [rating, setRating] = useState(0)
+    const dispatch = useDispatch();
+
+    const handleStarClick = (value) => {
+        setRating(value);
+      };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            This is Post Review
+        <div className="post-modal">
+            <h1 className="review-header">How was your stay?</h1>
+            <textarea
+                className="stay-describe"
+                placeholder="Leave your review here . . . . "
+                value={null}
+                onChange={(e) => setReview(e.target.value)}
+            />
+            <div className="star-rating">
+                <span className={rating >= 1 ? "star-filled" : "star-empty"} onClick={() => handleStarClick(1)}>★</span>
+                <span className={rating >= 2 ? "star-filled" : "star-empty"} onClick={() => handleStarClick(2)}>★</span>
+                <span className={rating >= 3 ? "star-filled" : "star-empty"} onClick={() => handleStarClick(3)}>★</span>
+                <span className={rating >= 4 ? "star-filled" : "star-empty"} onClick={() => handleStarClick(4)}>★</span>
+                <span className={rating >= 5 ? "star-filled" : "star-empty"} onClick={() => handleStarClick(5)}>★</span>
+                <span className="star-text">Stars</span>
+            </div>
+            <button
+                className="submit-review"
+                disabled={rating === 0 || review.length < 10}
+                onClick={handleSubmit}>
+                    Submit Review
+            </button>
         </div>
     )
 }
